355f3f444d1576daa1192db08fe14ead4f7979d5
Vianpyro <vianney.veremme@gmail.com>
2023-09-30 14:29:53 -0400

    Utilisation de constantes symboliques

--- Semaine 1/3-somme.c
+++ Semaine 1/3-somme.c
@@ -1,5 +1,5 @@
 /*
-    �crivez une fonction "main" qui permet de calculer la somme de plusieurs entiers saisis au clavier et compris dans l'intervalle 1 � 10.
+    Ecrire une fonction "main" qui permet de calculer la somme de plusieurs entiers saisis au clavier et compris dans l'intervalle 1 a 10.
     Pour ce faire,  d�clarez deux variables `nombre` et `somme` de type entier long ainsi que trois constantes FIN, MIN et MAX ayant les valeurs 0, 1 et 10.
     Ensuite, tant que nombre n'est pas �gal � FIN, saisissez un nombre au clavier (une valeur enti�re que vous mettez dans nombre).
     Si le nombre est dans l'intervalle MIN � MAX, ajoutez ce nombre a la somme. Sinon, si le nombre n'est pas �gal � FIN, affichez un message d'erreur. 
@@ -10,9 +10,11 @@
 #include <stdio.h> // #include <stdio.h> pour les fonctions d'entr�e/sortie
 #include <stdlib.h> // Pour les fonctions d'entr�e/sortie et la fonction `system()`
 
+#define FIN 0
+#define MIN 1
+#define MAX 10
+
 int main() {
-    int FIN = 0, MIN = 1, MAX = 10;
-    
     long nombre, somme = 0;
 
     while (1) {
@@ -30,4 +32,5 @@ int main() {
     }
 
     system("pause");
+    return EXIT_SUCCESS;
 }
