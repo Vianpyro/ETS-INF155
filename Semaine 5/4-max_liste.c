969bcd02fab6c3262f49ad77c7fce99d62e9c79a
Vianpyro <vianney.veremme@gmail.com>
2023-10-04 19:21:38 -0400

    Ajout exercice 4

---
+++ Semaine 5/4-max_liste.c
@@ -0,0 +1,31 @@
+/*
+    Ecrire une fonction qui recoit en parametres un tableau d'entiers ainsi que le nombre d'éléments du tableau.
+    La fonction retourne l'indice du plus grand element.
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NB_ELEMENTS 10
+
+int indice_maximum(int tableau[], int longueur);
+
+int main(void) {
+    int tableau[NB_ELEMENTS] = {-1, 56, 47, -96, 24, 18, 12, 14, 0, -1};
+
+    printf("Le plus grand element se trouve a l'indice %d.\n", indice_maximum(tableau, NB_ELEMENTS));
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
+
+int indice_maximum(int tableau[], int longueur) {
+    int index = 0;
+
+    for (int i = 1; i < longueur; i++) {
+        if (tableau[i] > tableau[index])
+            index = i;
+    } 
+
+    return index;
+}
