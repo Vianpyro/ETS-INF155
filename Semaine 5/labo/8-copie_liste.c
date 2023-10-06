7cf191b59fe496dc34ffe403acec2d126747447f
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2023-10-06 15:36:40 -0400

    Create 8-copie_liste.c

---
+++ Semaine 5/labo/8-copie_liste.c
@@ -0,0 +1,41 @@
+/*
+    Ecrire une fonction qui copie un tableau dans un autre.
+    La fonction reçoit en parametres deux tableaux ainsi que le nombre d'elements a copier.
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+
+#define MAX 100
+#define MAX_ELEMENTS 10
+
+void copier_tableau(int *dest[], const int source[], int nb_elem);
+int nombre_aleatoire(int min, int max);
+
+int main(void) {
+    int tableau_source[MAX_ELEMENTS],
+        tableau_destination[MAX_ELEMENTS];
+
+    // Definition des valeurs du premier tableau
+    srand(time(NULL));
+    for (short i = 0; i < MAX_ELEMENTS; i++)
+        tableau_source[i] = nombre_aleatoire(0, MAX);
+
+    // Copie du tableau de source dans le tableau de destination
+    copier_tableau(&tableau_destination, tableau_source, MAX_ELEMENTS);
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
+
+void copier_tableau(int *dest[], const int source[], int nb_elem) {
+    for (int i = 0; i < nb_elem; i++) {
+        *dest[i] = source[i];
+    }
+}
+
+int nombre_aleatoire(int min, int max) {
+    return (rand() % (max - min + 1) + min);
+}
