9b6d1a45455a183325ceb11436388db0c64d2d8b
Vianpyro <vianney.veremme@gmail.com>
2023-10-06 16:43:38 -0400

    Create 9-inversion_liste.c

---
+++ Semaine 5/labo/9-inversion_liste.c
@@ -0,0 +1,53 @@
+/*
+    Ecrire une fonction qui renverse un tableau.
+    Le premier element devient le dernier, le dernier devient le premier, le second devient l'avant dernier, etc.
+    La fonction reçoit en parametres le tableau a renverser ainsi que le nombre d'elements dans le tableau.
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+#define MAX_ELEMENTS 10
+
+void renverser_tableau(int tableau[], int nb_elem);
+int nombre_aleatoire(int min, int max);
+
+int main(void) {
+    int tableau[MAX_ELEMENTS];
+
+    // Definition des valeurs du tableau
+    srand(time(NULL));
+    for (short i = 0; i < MAX_ELEMENTS; i++)
+        tableau[i] = nombre_aleatoire(0, MAX);
+
+    // Affichage du tableau avant inversion
+    for (short i = 0; i < MAX_ELEMENTS; i++)
+        printf("%d ", tableau[i]);
+    printf("\n");
+
+    // Inversion du tableau
+    renverser_tableau(tableau, MAX_ELEMENTS);
+
+    for (short i = 0; i < MAX_ELEMENTS; i++)
+        printf("%d ", tableau[i]);
+    printf("\n");
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
+
+// Fonction d'inversion du tableau
+void renverser_tableau(int tableau[], int nb_elem) {
+    int temp;
+
+    for (int i = 0; i < nb_elem / 2; i++) {
+        int temp = tableau[i];
+        tableau[i] = tableau[nb_elem - 1 - i];
+        tableau[nb_elem - 1 - i] = temp;
+    }
+}
+
+int nombre_aleatoire(int min, int max) {
+    return (rand() % (max - min + 1) + min);
+}
