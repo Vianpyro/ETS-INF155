5769805565ce08235fffe490369e6d0276b45294
Vianpyro <vianney.veremme@gmail.com>
2023-10-06 15:55:39 -0400

    Correction

--- Semaine 5/labo/8-copie_liste.c
+++ Semaine 5/labo/8-copie_liste.c
@@ -5,13 +5,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <assert.h>
 
 #define MAX 100
 #define MAX_ELEMENTS 10
 
-void copier_tableau(int *dest[], const int source[], int nb_elem);
+void copier_tableau(int dest[], const int source[], int nb_elem);
 int nombre_aleatoire(int min, int max);
 
 int main(void) {
@@ -24,16 +22,18 @@ int main(void) {
         tableau_source[i] = nombre_aleatoire(0, MAX);
 
     // Copie du tableau de source dans le tableau de destination
-    copier_tableau(&tableau_destination, tableau_source, MAX_ELEMENTS);
+    copier_tableau(tableau_destination, tableau_source, MAX_ELEMENTS);
+
+    for (short i = 0; i < MAX_ELEMENTS; i++)
+        printf("%d\n", tableau_destination[i]);
 
     system("pause");
     return EXIT_SUCCESS;
 }
 
-void copier_tableau(int *dest[], const int source[], int nb_elem) {
-    for (int i = 0; i < nb_elem; i++) {
-        *dest[i] = source[i];
-    }
+void copier_tableau(int dest[], const int source[], int nb_elem) {
+    for (int i = 0; i < nb_elem; i++)
+        dest[i] = source[i];
 }
 
 int nombre_aleatoire(int min, int max) {
