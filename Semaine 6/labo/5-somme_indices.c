79c0b4446be7dcbed3ca5e0c0e61bde671d7e96d
Vianpyro <vianney.veremme@gmail.com>
2023-10-20 20:23:34 -0400

    Ajout exercice 5 labo 6

---
+++ Semaine 6/labo/5-somme_indices.c
@@ -0,0 +1,55 @@
+/*
+    Ecrire un programme qui declare un tableau d'entiers de 10 lignes et 20 colonnes.
+    Le programme met la somme des indices dans chaque case du tableau (l'element
+        a la ligne 3 colonne 4 prend la valeur 7, l'element a la ligne 5 colonne
+        10 prend la valeur 15, etc...).
+    Finalement, le programme fait une copie du tableau et affiche la copie sur
+        10 lignes et 20 colonnes.
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NB_LIGNES 10
+#define NB_COLONNES 20
+
+void copier_tableau_2D(int destination[NB_LIGNES][NB_COLONNES],
+                       int source[NB_LIGNES][NB_COLONNES]);
+void affichage_tableau_2d(int tableau[NB_LIGNES][NB_COLONNES]);
+
+int main(void) {
+    int tableau[NB_LIGNES][NB_COLONNES],
+        copie[NB_LIGNES][NB_COLONNES];
+
+    // Remplissage
+    for (int i = 0; i < NB_LIGNES; i++) {
+        for (int j = 0; j < NB_COLONNES; j++) {
+            tableau[i][j] = i + j;
+        }
+    }
+
+    // Copie et affichage de la copie
+    copier_tableau_2D(copie, tableau);
+    affichage_tableau_2d(copie);
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
+
+void copier_tableau_2D(int destination[NB_LIGNES][NB_COLONNES],
+                       int source[NB_LIGNES][NB_COLONNES]) {
+    for (int i = 0; i < NB_LIGNES; i++) {
+        for (int j = 0; j < NB_COLONNES; j++) {
+            destination[i][j] = source[i][j];
+        }
+    }
+}
+
+void affichage_tableau_2d(int tableau[NB_LIGNES][NB_COLONNES]) {
+    for (int i = 0; i < NB_LIGNES; i++) {
+        for (int j = 0; j < NB_COLONNES; j++) {
+            printf(" %3d,", tableau[i][j]);
+        }
+        printf("\n");
+    }
+}
