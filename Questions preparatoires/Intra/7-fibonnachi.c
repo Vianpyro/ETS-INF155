784e7d57710edc2123345921219d71ef82f69b9d
Vianpyro <vianney.veremme@gmail.com>
2023-10-24 22:28:47 -0400

    Ajout exercice 7 preparation intra

---
+++ Questions preparatoires/Intra/7-fibonnachi.c
@@ -0,0 +1,30 @@
+/*
+    Programme de calcul de la suite de fibonnachi
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int fibo(int n);
+
+int main(void) {
+    int valeur;
+
+    assert(fibo(2) == 1);
+    assert(fibo(8) == 21);
+    assert(fibo(9) == 34);
+
+    printf("Entrez le numero du terme de la suite de fibonnachi que vous voulez: ");
+    scanf("%d", &valeur);
+
+    valeur = fibo(valeur);
+    printf("%d", valeur);
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
+
+int fibo(int n) {
+    return (n <= 2) ? 1 : fibo(n - 1) + fibo(n - 2);
+}
