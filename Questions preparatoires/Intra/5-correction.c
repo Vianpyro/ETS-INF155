9aa6444242d71727924619e62310752a5733e799
Vianpyro <vianney.veremme@gmail.com>
2023-10-24 21:02:05 -0400

    Ajout exercice 5 entrainement intra

---
+++ Questions preparatoires/Intra/5-correction.c
@@ -0,0 +1,28 @@
+/*
+    Corriger toutes les erreurs dans ce bloc et expliquer :
+
+    #define MAX 10
+
+    void main( ) {
+        int a;
+        scanf("%d", a);
+        MAX = 2*a;
+        printf("resultat = %d", MAX);
+    }
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 10
+
+int main(void) {  // La fonction main ne retourne rien "void"
+    int a;
+
+    scanf("%d", &a);  // On donne l'adresse (pointeur) pour stocker une valeur
+    // MAX = 2 * a;   // On ne peut tout simplement pas multiplier une macro
+
+    printf("resultat = %d\n", a);  // On veut afficher la variable a, pas la macro MAX
+
+    return EXIT_SUCCESS;  // On retourne 0 (pas de probleme)
+}
