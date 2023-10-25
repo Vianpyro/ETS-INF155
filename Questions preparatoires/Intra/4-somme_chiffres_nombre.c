d5d29feeaad5dbeccb423016a2f50398b8c7e6ee
Vianpyro <vianney.veremme@gmail.com>
2023-10-24 21:00:54 -0400

    Ajout exercice 4 entrainement intra

---
+++ Questions preparatoires/Intra/4-somme_chiffres_nombre.c
@@ -0,0 +1,23 @@
+/*
+    Somme des chiffres d'un nombre
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int x, tot = 0;
+
+    printf("Entrez une valeur: ");
+    scanf("%d", &x);
+
+    while (x) {
+        tot += x % 10;
+        x /= 10;
+    }
+
+    printf("La somme de ses chiffres est: %d\n", tot);
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
