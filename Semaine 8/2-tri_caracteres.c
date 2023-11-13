ff8a15cc5b626e562ac37fa2548fa7267fa16642
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2023-11-13 10:09:35 -0500

    Ajout exercice 2 semaine 8

---
+++ Semaine 8/2-tri_caracteres.c
@@ -0,0 +1,31 @@
+/*
+    But du programme: Saisie d'une chaine de caracteres au clavier, affichage des chiffres seulement
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_CHAR 80
+
+int main(void) {
+    int valeur,
+        somme = 0,
+        i = 0,
+        c;
+    char caracteres[MAX_CHAR];
+
+    printf("Entrez la chaine de caracteres: ");
+    scanf("%s", caracteres);
+
+    while ((c = caracteres[i]) != '\0') {
+        if ((c >= '0') && (c <= '9')) {
+            somme += c - (int)'0';  // Ajout de la difference des codes ASCII
+        }
+        i++;
+    }
+
+    printf("La somme est: %d\n", somme);
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
